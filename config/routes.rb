474c7ff17df3e4b242c5464efbd2d8229cdb40c8
PedroYouse <pedro.oliveira@youse.com.br>
2023-05-10 15:48:22 -0300

    db/routes/validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :contacts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
 
   get 'contato', to: 'static_pages#contato'
+
+  get 'entrar', to: 'sessions#new'
+
+  post 'entrar', to: 'session#create'
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 
 end
